f4c897327f7ed47914e46da166501f2b19d3b415
Eduardo Backes <e.backes2011@gmail.com>
2020-02-27 22:37:26 -0300

    Recebe as propriedades do box, popula o textarea e copia o css

--- src/css-preview/index.js
+++ src/css-preview/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
 
 import './style.css';
 
-function CssPreview() {
+function CssPreview(props) {
+  const { value } = props;
+
+  const textareaRef = useRef();
+
+  function handleCopyCss() {
+    const { current } = textareaRef;
+
+    current.select();
+    current.setSelectionRange(0, 99999);
+
+    document.execCommand('copy');
+  }
+
   return (
     <div className="css-preview">
-      <textarea></textarea>
+      <button
+        type="button"
+        className="css-preview__button"
+        onClick={handleCopyCss}
+      >
+        Copiar CSS
+      </button>
+      <textarea defaultValue={value} ref={textareaRef}></textarea>
     </div>
   );
 }
+
+CssPreview.propTypes = {
+  value: PropTypes.string.isRequired
+};
 
 export default CssPreview;
